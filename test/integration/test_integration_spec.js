47b8a70411704bb5945e6047e4f9ae862ed40882
Payam Moghaddam <payam.moghaddam@accenture.com>
2013-12-13 18:41:56 -0800

    Added test for MongoDb connection

--- test/integration/test_integration_spec.js
+++ test/integration/test_integration_spec.js
@@ -1,15 +1,21 @@
 'use strict';
 
 require('../../server');
+
 var request = require('request');
 var url = 'http://localhost:' + 5001;
+var mongoose = require('mongoose');
 
 describe('Basic integration test', function () {
-    it('should pass', function (done) {
+    it('should contact server', function (done) {
         request(url, function (error, response) {
             assert.ok(response);
             done();
         });
+    });
+
+    it('should connect to MongoDb', function() {
+        assert(mongoose.connection.name === 'blpm-test');
     });
 });
 
