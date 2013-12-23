ee6a40689f49185bb3e010dd9d89bdd1c1605a5c
Payam Moghaddam <payam.moghaddam@accenture.com>
2013-12-23 02:24:55 -0800

    Enhanced Task's schema

--- app/models/task.js
+++ app/models/task.js
@@ -7,19 +7,51 @@ var Schema = mongoose.Schema;
  * Schema
  */
 var taskSchema = new Schema({
-    created: {
-        type: Date,
-        default: Date.now
-    },
-    title: {
-        type: String,
-        default: '',
-        trim: true
-    },
     user: {
         type: Schema.ObjectId,
         ref: 'User',
         required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    title: {
+        type: String,
+        default: '',
+        required: true
+    },
+    deadlineAt: {
+        type: Date
+    },
+    status: {
+        type: String,
+        enum: ['notStarted', 'inProgress', 'completed'],
+        default: 'notStarted',
+        required: true
+    },
+    priority: {
+        type: Number,
+        default: 0
+    },
+    // Longer description than what title offers
+    description: {
+        type: String
+    },
+    // Additional box to capture extra information
+    notes: {
+        type: String
+    },
+    // Tasks should not be immediately deleted (needed for sync purposes)
+    deleted: {
+        type: Boolean,
+        default: false
     }
 });
 
